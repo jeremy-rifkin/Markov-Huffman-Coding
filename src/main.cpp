c52be2ca208ab9adf637848135f07d2f2576cbf8
Jeremy M. Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-01-20 20:52:48 -0700

    refactoring and command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <vector>
+#include <functional>
 
 #include "min_pq.h"
+
+#define null NULL
 
 #define BUFFER_SIZE 32768
 
@@ -14,6 +18,16 @@ void print_counts(int* counts) {
 			printf("%c %d\n", i, counts[i]);
 		}
 	}
+}
+
+void print_help() {
+	printf("markov-huffman <input> [-o output] [options]\n");
+	printf("\t-o output_file\n");
+	printf("\t-e encoding_file\n");
+	printf("\t-d output_encoding_file\n");
+	printf("\t-g debug / demo encoding\n");
+	printf("\t-h just use naive huffman\n");
+	printf("\t-x extract\n");
 }
 
 struct tree_node {
@@ -27,8 +41,8 @@ public:
 		left(left), right(right), is_internal(is_internal), value(value), weight(weight) {};
 	tree_node() {
 		is_internal = false;
-		left = NULL;
-		right = NULL;
+		left = null;
+		right = null;
 	}
 	tree_node(tree_node* l, tree_node* r) {
 		is_internal = true;
@@ -59,7 +73,7 @@ private:
 	// passed the next node number
 	// returns the node number left off at
 	int print_nodes(tree_node* node, int n) {
-		if(node == NULL) return -1;
+		if(node == null) return -1;
 		printf("\tn%d;\n", n);
 		if(node->is_internal) {
 			printf("\tn%d [label=\"%d\"];\n", n, node->weight);
@@ -103,59 +117,39 @@ struct encoding_descriptor {
 	}
 };
 
-class huffman_table {
+class i_coding_provider {
+public:
+	//virtual int get_type() = 0;
+	virtual void print_table() = 0;
+	virtual void print_tree() = 0;
+	//virtual void get_encoding() = 0;
+	//virtual void decode() = 0;
+};
+
+class huffman_table: public i_coding_provider {
 	tree_node* huffman_tree;
 	encoding_descriptor encoding_table[256];
 	int counts[256];
 	int total = 0;
 public:
-	huffman_table() : huffman_tree { NULL } {
+	huffman_table(int* counts) : huffman_tree { null } {
 		for(int i = 0; i < 256; i++) {
-			counts[i] = 0;
+			this->counts[i] = counts[i];
+			total += counts[i];
 		}
+		build();
 	}
-	void increment(int c) {
+	[[deprecated]] void increment(int c) {
 		counts[c]++;
 		total++;
 	}
 	bool empty() {
 		return total == 0;
 	}
-	void build() {
-		if(empty()) {
-			return;
-		}
-		// build huffman tree from the counts
-		min_pq<int, tree_node*> q;
-		for(int i = 0; i < 256; i++) {
-			if(counts[i]) {
-				q.insert(counts[i], new tree_node { i, counts[i] });
-			}
-		}
-		while(q.size() > 1) {
-			tree_node* a = q.pop_min();
-			tree_node* b = q.pop_min();
-			q.insert(a->weight + b->weight, new tree_node { a, b });
-		}
-		huffman_tree = q.pop_min();
-		// edge case where the tree has height 0
-		if(!huffman_tree->is_internal) {
-			// TODO: weights
-			huffman_tree->left  = new tree_node { NULL, NULL, false, huffman_tree->value, huffman_tree->weight };
-			//huffman_tree->right = new tree_node { NULL, NULL, false, huffman_tree->value, 0 };
-			huffman_tree->is_internal = true;
-		}
-		// recursive builder will continuously update this descriptor to build the tree
-		encoding_descriptor working_descriptor;
-		build_huffman_encoding_table(huffman_tree, working_descriptor);
-		huffman_tree->print();
-		print_encoding_table();
+	int get_total() {
+		return total;
 	}
-	void rebuild() {
-		delete huffman_tree;
-		build();
-	}
-	void print_encoding_table() {
+	virtual void print_table() override {
 		printf("Table:\n");
 		for(int i = 0; i < 256; i++) {
 			if(encoding_table[i].length) {
@@ -169,9 +163,12 @@ public:
 			}
 		}
 	}
+	virtual void print_tree() override {
+		huffman_tree->print();
+	}
 private:
 	void build_huffman_encoding_table(tree_node* node, encoding_descriptor& descriptor) {
-		if(node == NULL) return;
+		if(node == null) return;
 		if(node->is_internal) {
 			descriptor.push_bit(0);
 			build_huffman_encoding_table(node->left, descriptor);
@@ -183,42 +180,193 @@ private:
 			encoding_table[node->value] = descriptor;
 		}
 	}
+	void build() {
+		if(empty()) {
+			return;
+		}
+		// build huffman tree from the counts
+		min_pq<int, tree_node*> q;
+		for(int i = 0; i < 256; i++) {
+			if(counts[i]) {
+				q.insert(counts[i], new tree_node { (unsigned char) i, counts[i] });
+			}
+		}
+		while(q.size() > 1) {
+			tree_node* a = q.pop_min();
+			tree_node* b = q.pop_min();
+			q.insert(a->weight + b->weight, new tree_node { a, b });
+		}
+		huffman_tree = q.pop_min();
+		// edge case where the tree has height 0
+		if(!huffman_tree->is_internal) {
+			// TODO: weights
+			huffman_tree->left  = new tree_node { null, null, false, huffman_tree->value, huffman_tree->weight };
+			//huffman_tree->right = new tree_node { null, null, false, huffman_tree->value, 0 };
+			huffman_tree->is_internal = true;
+		}
+		// recursive builder will continuously update this descriptor to build the tree
+		encoding_descriptor working_descriptor;
+		build_huffman_encoding_table(huffman_tree, working_descriptor);
+		//huffman_tree->print();
+		//print_encoding_table();
+	}
 };
 
-class markov_huffman_table {
-	huffman_table* tables;
+class markov_huffman_table: public i_coding_provider {
+	huffman_table** tables;
+	int totals[256];
 public:
-	markov_huffman_table() {
-		tables = new huffman_table[256];
-	}
-	void increment(int c, int prev) {
-		tables[prev].increment(c);
-	}
-	void build() {
+	markov_huffman_table(int* counts) {
+		tables = new huffman_table*[256];
 		for(int i = 0; i < 256; i++) {
-			if(!tables[i].empty())
-				printf("[[building '%c']]\n", i);
-			tables[i].build();
+			tables[i] = new huffman_table(counts + 256 * i);
+			totals[i] = tables[i]->get_total();
+		}
+	}
+	virtual void print_table() override {
+		for(int i = 0; i < 256; i++) {
+			printf("Prev '%c' table:\n", i);
+			tables[i]->print_table();
+		}
+	}
+	virtual void print_tree() override {
+		// TODO: subgraphs.
+		for(int i = 0; i < 256; i++) {
+			printf("Prev '%c' table:\n", i);
+			tables[i]->print_table();
 		}
 	}
 };
 
-int main() {
-	char* input = "input_a.txt";
-	FILE* input_fd = fopen(input, "r");
+void construct_table(FILE* input_fd, std::function<void(unsigned char, unsigned char)> counter) {
 	size_t bytes_read;
 	unsigned char buffer[BUFFER_SIZE];
-	printf("%d %d\n", sizeof(huffman_table), sizeof(markov_huffman_table));
-	huffman_table h_table;
-	markov_huffman_table mh_table;
 	int prev = ' ';
 	while(bytes_read = fread(buffer, 1, BUFFER_SIZE, input_fd)) {
 		for(int i = 0; i < bytes_read; i++) {
-			h_table.increment(buffer[i]);
-			mh_table.increment(buffer[i], prev);
+			counter(prev, buffer[i]);
 			prev = buffer[i];
 		}
 	}
+}
+
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		print_help();
+		return 1;
+	}
+	// Parameters
+	bool extract = false;
+	bool debug = false;
+	bool simple_huffman = false;
+	char* input = null;
+	char* encoding = null;
+	char* output = null;
+	char* encoding_output = null;
+	// Process arguments
+	for(int i = 1; i < argc; i++) {
+		if(argv[i][0] == '-') {
+			int chomp = 0;
+			for(int j = 0; argv[i][++j] != 0x0; )
+				switch(argv[i][j]) {
+					case 'o':
+						if(i + 1 < argc) {
+							output = argv[i + chomp++ + 1];
+						} else {
+							fprintf(stderr, "[Error] Expected output file following -o.\n");
+						}
+						break;
+					case 'e':
+						if(i + 1 < argc) {
+							encoding = argv[i + chomp++ + 1];
+						} else {
+							fprintf(stderr, "[Error] Expected encoding file following -e.\n");
+						}
+						break;
+					case 'd':
+						if(i + 1 < argc) {
+							encoding_output = argv[i + chomp++ + 1];
+						} else {
+							fprintf(stderr, "[Error] Expected encoding output file following -d.\n");
+						}
+						break;
+					case 'x':
+						extract = true;
+						break;
+					case 'h':
+						simple_huffman = true;
+						break;
+					case 'g':
+						debug = true;
+						break;
+					default:
+						fprintf(stderr, "[Warning] Unknown option %c\n", argv[i][j]);
+				}
+			i += chomp;
+		} else {
+			if(input == null) {
+				input = argv[i];
+			} else {
+				fprintf(stderr, "[Warning] Unknown positional argument %s\n", argv[i]);
+			}
+		}
+	}
+
+	if(input == null) {
+		printf("error must provide input\n");
+		exit(1);
+	}
+
+	FILE* input_fd = fopen(input, "r");
+	if(input_fd == null) {
+		printf("error opening input errno %s\n", strerror(errno));
+		exit(1);
+	}
+
+	// file needs to indicate whether it has the encoding embedded (todo: good idea or not??)
+	// file needs to indicate whether it is naive huffman or fancy huffman
+	i_coding_provider* coder = null;
+	if(encoding) {
+		// load encoding
+		fprintf(stderr, "--error--\n");
+	} else {
+		// build encoding
+		if(simple_huffman) {
+			int counts[256];
+			for(int i = 0; i < 256; i++) counts[i] = 0;
+			construct_table(input_fd, [&](unsigned char prev, unsigned char c) {
+				counts[c]++;
+			});
+			coder = new huffman_table(counts);
+		} else {
+			int* counts = new int[256 * 256];
+			for(int i = 0; i < 256 * 256; i++) counts[i] = 0;
+			construct_table(input_fd, [&](unsigned char prev, unsigned char c) {
+				counts[256 * prev + c]++;
+			});
+			coder = new markov_huffman_table(counts);
+		}
+		fseek(input_fd, 0, SEEK_SET);
+	}
+
+	if(debug) {
+		coder->print_table();
+		coder->print_tree();
+	}
+
+	if(extract) {
+		fprintf(stderr, "--error--\n");
+	} else {
+		if(debug) {
+
+		} else {
+			
+		}
+	}
+
+	/*char* input = "input_a.txt";
+	FILE* input_fd = fopen(input, "r");
+	
 	h_table.build();
 	mh_table.build();
 	//print_counts(counts);
@@ -235,6 +383,6 @@ int main() {
 	//	}
 	//}
 
-	fclose(input_fd);
+	fclose(input_fd);*/
 }
 
