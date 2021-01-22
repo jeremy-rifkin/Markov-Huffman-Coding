40a14b56132bdc0ff7df6d54d1d641f4ca8969ad
Jeremy M. Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-01-21 19:48:14 -0700

    moved encoder to separate function

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,114 @@ void print_help() {
 	printf("\t-x extract\n");
 }
 
+/*
+ * Output file format:
+ * [metadata: 1byte] [data: .........]
+ * metadata:
+ *  1 bit complete | 3 bits unused | 1 bit encoder | 3 bits remainder
+ *  complete: 1 if the header is currently blank, 0 if it the header is valid
+ *  encoder: Indicates the encoding method and stores the inverse of get_type. I'm storing the
+ *           inverse here so I can do a cool ascii hack.
+ *  remainder: Number of bits of the last byte unused
+ * 0 0 1  1 E R R R
+ *    32 16 8 4 2 1
+ * The unused bits take value 0x30 so that with markov-huffman coding, the ascii value of the
+ * header byte itself takes on a '0' to '9' value equal to the remainder.
+ * The data length in bits can be found from the file length and partial byte.
+ * TODO: Currently have to seek back to write the last byte. Consider putting header byte at the
+ * end...
+ * TODO: CRC? Probably not needed for this proof of concept..
+ *
+ * NOTE: right bytes are filled with codewords left to right. Math might be simpler if filled
+ * from right to left...
+ * TODO: clean up all this math
+ * TODO: get rid of all asserts
+ */
+
+void compress(i_coding_provider* coder, FILE* input_fd, FILE* output_fd) {
+	size_t bytes_read;
+	unsigned char input_buffer[BUFFER_SIZE];
+	unsigned char output_buffer[BUFFER_SIZE] = { 1<<7 };
+	int output_buffer_index = 1; // skip header byte
+	unsigned char byte = 0x0;
+	int output_bit_index = 0;
+	int prev = ' ';
+	while(bytes_read = fread(input_buffer, 1, BUFFER_SIZE, input_fd)) {
+		for(int input_buffer_index = 0; input_buffer_index < bytes_read; input_buffer_index++) {
+			// get encoding for character in input
+			encoding_descriptor& e = coder->get_encoding(prev, input_buffer[input_buffer_index]);
+			printf("[encoding character '%c': %d ", input_buffer[input_buffer_index], e.length);
+			e.print();
+			printf("]\n");
+			// update state
+			prev = input_buffer[input_buffer_index];
+			// write encoding
+			for(int i = 0; i < (e.length + 7) / 8; i++) {
+				// how many bits we're working with this loop
+				int working_bits = std::min(8, e.length - i * 8);
+				print_byte(byte); printf("\n");
+				for(int _ = 0; _ < output_bit_index; _++) printf(" "); printf("^\n");
+				// three cases
+				if(output_bit_index + working_bits == 8) { // fits perfectly
+					// insert
+					byte |= e.encoding[i] >> (8 - working_bits);
+					printf("--fits perfectly--\n"); print_byte(byte); printf("\n");
+					// flush
+					output_buffer[output_buffer_index++] = byte;
+					// reset
+					byte = 0;
+					output_bit_index = 0;
+				} else if(output_bit_index + working_bits < 8) { // falls short of fitting
+					//byte |= (e.encoding[i] >> (8 - working_bits)) << (8 - (output_bit_index + working_bits));
+					byte |= e.encoding[i] >> output_bit_index;
+					output_bit_index += working_bits;
+					printf("--under fills--\n"); print_byte(byte); printf("\n");
+					for(int _ = 0; _ < output_bit_index; _++) printf(" "); printf("^\n");
+					continue; // skip the output_buffer_index check TODO: micro-optimization?
+				} else { // code exceeds byte size
+					// fill
+					//byte |= (e.encoding[i] >> (8 - (8 - output_bit_index)));
+					byte |= e.encoding[i] >> output_bit_index;
+					printf("--over fills--\n");
+					printf("first byte:\n");
+					print_byte(byte); printf("\n");
+					// flush
+					output_buffer[output_buffer_index++] = byte;
+					// insert and reset
+					byte = e.encoding[i] << (8 - output_bit_index);
+					output_bit_index = working_bits - (8 - output_bit_index);
+					printf("--partial byte--\n"); print_byte(byte); printf("\n");
+					for(int _ = 0; _ < output_bit_index; _++) printf(" "); printf("^\n");
+				}
+				// flush buffer if necessary
+				if(output_buffer_index == BUFFER_SIZE) {
+					//fwrite(output_buffer, 1, BUFFER_SIZE, output_fd);
+					output_buffer_index = 0;
+				}
+			}
+		}
+	}
+	// Check for read errors
+	if(bytes_read == -1) {
+		fprintf(stderr, "error occurred while reading input (errno: %d).\n", errno);
+		exit(1);
+	}
+	// remainder byte
+	if(output_bit_index > 0)
+		output_buffer[output_buffer_index++] = byte;
+	// write whatever is left
+	if(output_buffer_index > 0)
+		assert(fwrite(output_buffer, 1, output_buffer_index, output_fd) == output_buffer_index);
+	// go back and write header....
+	fseek(output_fd, 0, SEEK_SET);
+	assert(output_bit_index >= 0 && output_bit_index < 8);
+	printf("--remainder:-- %d\n", (8 - output_bit_index) % 8);
+	printf("--type:-- %d\n", coder->get_type());
+	printf("--type:-- %d\n", (~coder->get_type() & 1) << 3);
+	unsigned char header = 0x30 | (~coder->get_type() & 1) << 3 | (8 - output_bit_index) % 8;
+	assert(fwrite(&header, 1, 1, output_fd) == 1);
+}
+
 void construct_table(FILE* input_fd, std::function<void(unsigned char, unsigned char)> counter) {
 	size_t bytes_read;
 	unsigned char buffer[BUFFER_SIZE];
@@ -191,114 +299,11 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
-	/*
-	 * Output file format:
-	 * [metadata: 1byte] [data: .........]
-	 * metadata:
-	 *  1 bit complete | 3 bits unused | 1 bit encoder | 3 bits remainder
-	 *  complete: 1 if the header is currently blank, 0 if it the header is valid
-	 *  encoder: Indicates the encoding method and stores the inverse of get_type. I'm storing the
-	 *           inverse here so I can do a cool ascii hack.
-	 *  remainder: Number of bits of the last byte unused
-	 * 0 0 1  1 E R R R
-	 *    32 16 8 4 2 1
-	 * The unused bits take value 0x30 so that with markov-huffman coding, the ascii value of the
-	 * header byte itself takes on a '0' to '9' value equal to the remainder.
-	 * The data length in bits can be found from the file length and partial byte.
-	 * TODO: Currently have to seek back to write the last byte. Consider putting header byte at the
-	 * end...
-	 * TODO: CRC? Probably not needed for this proof of concept..
-	 *
-	 * NOTE: right bytes are filled with codewords left to right. Math might be simpler if filled
-	 * from right to left...
-	 * TODO: clean up all this math
-	 */
-
 	if(extract) {
 		fprintf(stderr, "--error--\n");
 		exit(1);
 	} else {
-		size_t bytes_read;
-		unsigned char input_buffer[BUFFER_SIZE];
-		unsigned char output_buffer[BUFFER_SIZE] = { 1<<7 };
-		int output_buffer_index = 1; // skip header byte
-		unsigned char byte = 0x0;
-		int output_bit_index = 0;
-		int prev = ' ';
-		while(bytes_read = fread(input_buffer, 1, BUFFER_SIZE, input_fd)) {
-			for(int input_buffer_index = 0; input_buffer_index < bytes_read; input_buffer_index++) {
-				// get encoding for character in input
-				encoding_descriptor& e = coder->get_encoding(prev, input_buffer[input_buffer_index]);
-				printf("[encoding character '%c': %d ", input_buffer[input_buffer_index], e.length);
-				e.print();
-				printf("]\n");
-				// update state
-				prev = input_buffer[input_buffer_index];
-				// write encoding
-				for(int i = 0; i < (e.length + 7) / 8; i++) {
-					// how many bits we're working with this loop
-					int working_bits = std::min(8, e.length - i * 8);
-					print_byte(byte); printf("\n");
-					for(int _ = 0; _ < output_bit_index; _++) printf(" "); printf("^\n");
-					// three cases
-					if(output_bit_index + working_bits == 8) { // fits perfectly
-						// insert
-						byte |= e.encoding[i] >> (8 - working_bits);
-						printf("--fits perfectly--\n"); print_byte(byte); printf("\n");
-						// flush
-						output_buffer[output_buffer_index++] = byte;
-						// reset
-						byte = 0;
-						output_bit_index = 0;
-					} else if(output_bit_index + working_bits < 8) { // falls short of fitting
-						//byte |= (e.encoding[i] >> (8 - working_bits)) << (8 - (output_bit_index + working_bits));
-						byte |= e.encoding[i] >> output_bit_index;
-						output_bit_index += working_bits;
-						printf("--under fills--\n"); print_byte(byte); printf("\n");
-						for(int _ = 0; _ < output_bit_index; _++) printf(" "); printf("^\n");
-						continue; // skip the output_buffer_index check TODO: micro-optimization?
-					} else { // code exceeds byte size
-						// fill
-						//byte |= (e.encoding[i] >> (8 - (8 - output_bit_index)));
-						byte |= e.encoding[i] >> output_bit_index;
-						printf("--over fills--\n");
-						printf("first byte:\n");
-						print_byte(byte); printf("\n");
-						// flush
-						output_buffer[output_buffer_index++] = byte;
-						// insert and reset
-						byte = e.encoding[i] << (8 - output_bit_index);
-						output_bit_index = working_bits - (8 - output_bit_index);
-						printf("--partial byte--\n"); print_byte(byte); printf("\n");
-						for(int _ = 0; _ < output_bit_index; _++) printf(" "); printf("^\n");
-					}
-					// flush buffer if necessary
-					if(output_buffer_index == BUFFER_SIZE) {
-						//fwrite(output_buffer, 1, BUFFER_SIZE, output_fd);
-						output_buffer_index = 0;
-					}
-				}
-			}
-		}
-		// Check for read errors
-		if(bytes_read == -1) {
-			fprintf(stderr, "error occurred while reading input (errno: %d).\n", errno);
-			exit(1);
-		}
-		// remainder byte
-		if(output_bit_index > 0)
-			output_buffer[output_buffer_index++] = byte;
-		// write whatever is left
-		if(output_buffer_index > 0)
-			assert(fwrite(output_buffer, 1, output_buffer_index, output_fd) == output_buffer_index);
-		// go back and write header....
-		fseek(output_fd, 0, SEEK_SET);
-		assert(output_bit_index >= 0 && output_bit_index < 8);
-		printf("--remainder:-- %d\n", (8 - output_bit_index) % 8);
-		printf("--type:-- %d\n", coder->get_type());
-		printf("--type:-- %d\n", (~coder->get_type() & 1) << 3);
-		unsigned char header = 0x30 | (~coder->get_type() & 1) << 3 | (8 - output_bit_index) % 8;
-		assert(fwrite(&header, 1, 1, output_fd) == 1);
+		compress(coder, input_fd, output_fd);
 	}
 	fclose(input_fd);
 	fclose(output_fd);
